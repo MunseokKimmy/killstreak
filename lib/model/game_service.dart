115b0d2053185a76f907b93b9fa4ab85412f0b17
Munseok Kim <59575386+PresideMoon@users.noreply.github.com>
2022-07-30 21:43:40 -0600

    Change game stuff.

--- lib/model/game_service.dart
+++ lib/model/game_service.dart
@@ -28,13 +28,13 @@ class GameService {
     gameShorts.add(GameShort("Playoffs Game 1", "Aldair's Volleyball Group",
         "Jul 29, 2022", "Red", "Blue", 25, 17, true, false, false));
     gameShorts.add(GameShort("Playoffs Game 2", "Aldair's Volleyball Group",
-        "Jul 29, 2022", "Yellow", "Green", 2, 1, true, true, true));
+        "Jul 29, 2022", "Yellow", "Green", 12, 25, true, true, true));
     gameShorts.add(GameShort("Playoffs Game 3", "RB", "Jul 29, 2022", "Grey",
-        "Black", 22, 11, false, false, false));
+        "Black", 25, 11, false, false, false));
     gameShorts.add(GameShort("Playoffs Game 4", "BYU Intermurals",
-        "Jul 29, 2022", "Orange", "Brown", 18, 5, true, true, false));
+        "Jul 29, 2022", "Orange", "Brown", 18, 25, true, true, false));
     gameShorts.add(GameShort("Playoffs Game 5", "47th Ward", "Jul 29, 2022",
-        "White", "Pink", 1, 7, false, false, false));
+        "White", "Pink", 25, 7, false, false, false));
   }
   List<GameShort> getGameShorts() {
     return gameShorts;
