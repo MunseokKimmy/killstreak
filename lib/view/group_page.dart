2fdc953ea8bdb3956583dde75938aa1eae86f63c
Munseok Kim <59575386+PresideMoon@users.noreply.github.com>
2022-08-02 19:42:35 -0600

    Add group page.

--- lib/view/group_page.dart
+++ lib/view/group_page.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/bottom_navigation.dart';
+
 class GroupPage extends StatelessWidget {
   const GroupPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Groups'),
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.info_outline),
+          ),
+        ],
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: Container(),
+      bottomNavigationBar: BottomNavigation(
+        currentPage: 0,
+        shortcut: true,
+      ),
+    );
   }
 }
