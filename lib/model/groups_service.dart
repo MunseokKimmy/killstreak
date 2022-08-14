07376e9516282a644aa50dae5afe746dfabdba3d
Munseok Kim <59575386+PresideMoon@users.noreply.github.com>
2022-08-13 18:31:42 -0600

    Finish top stats carousel.

--- lib/model/groups_service.dart
+++ lib/model/groups_service.dart
@@ -9,28 +9,38 @@ class GroupStatsSummary {
   String groupId;
   int gamesPlayed;
   String topAceName;
+  String topAceUserName;
   int topAceAmount;
   String topKillName;
+  String topKillUserName;
   int topKillAmount;
   String topAssistName;
+  String topAssistUserName;
   int topAssistAmount;
   String topBlockName;
+  String topBlockUserName;
   int topBlockAmount;
   String topDigName;
+  String topDigUserName;
   int topDigAmount;
   GroupStatsSummary(
       this.groupName,
       this.groupId,
       this.gamesPlayed,
       this.topAceName,
+      this.topAceUserName,
       this.topAceAmount,
       this.topKillName,
+      this.topKillUserName,
       this.topKillAmount,
       this.topAssistName,
+      this.topAssistUserName,
       this.topAssistAmount,
       this.topBlockName,
+      this.topBlockUserName,
       this.topBlockAmount,
       this.topDigName,
+      this.topDigUserName,
       this.topDigAmount);
 }
 
@@ -42,8 +52,25 @@ class GroupService {
     testGroups.add(Group("RB", "#4211"));
     testGroups.add(Group("BYU Intermurals", "#9412"));
     testGroups.add(Group("47th Ward", "#8080"));
-    groupStatsSummary = GroupStatsSummary("Aldair's Volleyball Group", "#6424",
-        725, "Lydia", 162, "Sara", 80, "Mariah", 67, "Jason", 40, "Alma", 100);
+    groupStatsSummary = GroupStatsSummary(
+        "Aldair's Volleyball Group",
+        "#6424",
+        725,
+        "Lydia",
+        "@lydia123",
+        162,
+        "Sara",
+        "@saradavis321",
+        80,
+        "Mariah",
+        "@mariahhh",
+        67,
+        "Jason",
+        "@therealjason",
+        40,
+        "Alma",
+        "@almaseo",
+        100);
   }
 
   List<Group> getGroups() {
--- lib/view/group_stats_page.dart
+++ lib/view/group_stats_page.dart
@@ -3,9 +3,13 @@
 // Games played
 // All other players' aces/kills/etc... in paid version
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:killstreak/utils/string.utils.dart';
+import 'package:killstreak/view/profile_page.dart';
 
+import '../main.dart';
 import '../model/groups_service.dart';
 import '../widgets/bottom_navigation.dart';
 
@@ -34,11 +38,7 @@ class GroupsStatsPage extends StatelessWidget {
         ),
       ),
       body: Container(
-        margin: const EdgeInsets.only(
-          left: 25,
-          right: 25,
-          bottom: 20,
-        ),
+        margin: const EdgeInsets.all(25),
         child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -51,13 +51,199 @@ class GroupsStatsPage extends StatelessWidget {
                 margin: const EdgeInsets.only(bottom: 20.0),
                 child: Text(DateFormat("MMMM dd yyyy").format(DateTime.now())),
               ),
-              GroupStatSummary(
-                groupStatsSummary: groupService.getSummary(),
-              ),
+              StatLeaderCarousel(),
+              // GroupStatSummary(
+              //   groupStatsSummary: groupService.getSummary(),
+              // ),
+              Text("asdfasdf"),
             ]),
       ),
       bottomNavigationBar: BottomNavigation(currentPage: 0, shortcut: false),
     );
+  }
+}
+
+class StatLeaderCarousel extends StatefulWidget {
+  StatLeaderCarousel({Key? key}) : super(key: key);
+  @override
+  State<StatLeaderCarousel> createState() => _StatLeaderCarouselState();
+}
+
+class _StatLeaderCarouselState extends State<StatLeaderCarousel> {
+  int _currentIndex = 0;
+  List cardList = [];
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    GroupService groupService = GroupService();
+    GroupStatsSummary statSummary = groupService.getSummary();
+    cardList = [
+      StatLeaderCard(
+        statLeaderName: statSummary.topAceName,
+        statLeaderUsername: statSummary.topAceUserName,
+        statName: "Ace",
+        statLeaderCount: statSummary.topAceAmount,
+        statImage: Image.asset(
+          'resources/images/ServiceColor.png',
+          height: 45,
+        ),
+      ),
+      StatLeaderCard(
+        statLeaderName: statSummary.topKillName,
+        statLeaderUsername: statSummary.topKillUserName,
+        statName: "Kills",
+        statLeaderCount: statSummary.topKillAmount,
+        statImage: Image.asset(
+          'resources/images/AttackColor.png',
+          height: 45,
+        ),
+      ),
+      StatLeaderCard(
+        statLeaderName: statSummary.topAssistName,
+        statLeaderUsername: statSummary.topAssistUserName,
+        statName: "Assists",
+        statLeaderCount: statSummary.topAssistAmount,
+        statImage: Image.asset(
+          'resources/images/AssistColor.png',
+          height: 45,
+        ),
+      ),
+      StatLeaderCard(
+        statLeaderName: statSummary.topBlockName,
+        statLeaderUsername: statSummary.topBlockUserName,
+        statName: "Blocks",
+        statLeaderCount: statSummary.topBlockAmount,
+        statImage: Image.asset(
+          'resources/images/BlockColor.png',
+          height: 45,
+        ),
+      ),
+      StatLeaderCard(
+        statLeaderName: statSummary.topDigName,
+        statLeaderUsername: statSummary.topDigUserName,
+        statName: "Digs",
+        statLeaderCount: statSummary.topDigAmount,
+        statImage: Image.asset(
+          'resources/images/DigColor.png',
+          height: 45,
+        ),
+      ),
+    ];
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            enlargeCenterPage: true,
+            enlargeStrategy: CenterPageEnlargeStrategy.scale,
+            viewportFraction: .33,
+            height: MediaQuery.of(context).size.height * .25,
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 10),
+            autoPlayAnimationDuration: const Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            pauseAutoPlayOnTouch: true,
+            aspectRatio: 16 / 5,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
+          items: cardList.map((card) {
+            return Builder(builder: (BuildContext context) {
+              return SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: card,
+              );
+            });
+          }).toList(),
+        ),
+      ],
+    );
+  }
+}
+
+class StatLeaderCard extends StatefulWidget {
+  String statLeaderName;
+  String statLeaderUsername;
+  String statName;
+  int statLeaderCount;
+  Image statImage;
+  StatLeaderCard(
+      {Key? key,
+      required this.statLeaderName,
+      required this.statLeaderUsername,
+      required this.statLeaderCount,
+      required this.statImage,
+      required this.statName})
+      : super(key: key);
+
+  @override
+  State<StatLeaderCard> createState() => _StatLeaderCardState();
+}
+
+class _StatLeaderCardState extends State<StatLeaderCard> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => ProfilePage()));
+        },
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          margin: EdgeInsets.only(bottom: 10),
+          decoration: BoxDecoration(
+            color: lightColor,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 2.0,
+                spreadRadius: 0.0,
+                offset: Offset(2.0, 2.0), // shadow direction: bottom right
+              )
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(child: widget.statImage, flex: 3),
+              Expanded(
+                flex: 1,
+                child: Text(
+                  widget.statName,
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Text(
+                  widget.statLeaderName,
+                  style: TextStyle(
+                      fontSize: 20, color: Color.fromARGB(255, 208, 208, 208)),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Text(
+                  widget.statLeaderCount.toString(),
+                  style: TextStyle(
+                      color: Colors.yellow,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
 
